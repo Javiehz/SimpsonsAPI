84d7a09d121bce8a80915989d0d829e773b62e4e
Javiehz <javier.encher@educa.jcyl.es>
2025-10-28 17:48:36 +0100

    Api Service

---
+++ app/src/main/java/edu/javieh/simpsonsapi/application/core/api/ApiClient.kt
@@ -0,0 +1,19 @@
+package edu.javieh.simpsonsapi.application.core.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class ApiClient {
+
+    val BASE_URL = "https://thesimpsonsapi.com/api/"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun <T> createService(typeClass:Class<T>):T{
+        return retrofit.create(typeClass)
+    }
+}
