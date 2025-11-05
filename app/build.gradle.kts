de7d84ac03d1faf85a8c88717ac583d2479440c2
Javiehz <javier.encher@educa.jcyl.es>
2025-11-05 12:13:49 +0100

    Domain layer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,9 @@ dependencies {
     //COROUTINES
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
 
+    //COIL
+    implementation(libs.coil)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
